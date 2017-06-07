28454d180fc985cd452c27c85d124944776dc19f
Deven <deven9852@gmail.com>
2017-06-07 21:30:21 +0530

    Completed dialog boxed and addition of elements

--- app/src/main/java/com/devapp/hp/expensemanagerrevamped/ExpenseFragment.java
+++ app/src/main/java/com/devapp/hp/expensemanagerrevamped/ExpenseFragment.java
@@ -1,15 +1,24 @@
 package com.devapp.hp.expensemanagerrevamped;
 
+import android.app.Dialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.Toast;
+
+import static com.devapp.hp.expensemanagerrevamped.MainActivity.Expense;
+import static com.devapp.hp.expensemanagerrevamped.MainActivity.ExpenseName;
 
 /**
  * Created by HP on 07-06-2017.
@@ -21,7 +30,7 @@ public class ExpenseFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
 
         View v = inflater.inflate(R.layout.expense_layout, container, false);
@@ -32,11 +41,42 @@ public class ExpenseFragment extends Fragment {
 
         rv.setLayoutManager(layoutManager);
 
-        RVAdapter adapter = new RVAdapter();
+        final RVAdapter adapter = new RVAdapter();
 
         rv.setAdapter(adapter);
 
-        Toast.makeText(getActivity() , "Hi there!", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(getActivity() , "Hi there!", Toast.LENGTH_SHORT).show();
+
+        FloatingActionButton fb = (FloatingActionButton) v.findViewById(R.id.floatingActionButton);
+
+        fb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+
+                new AlertDialog.Builder(v.getContext())
+                        .setIcon(android.R.drawable.ic_input_add)
+                        .setTitle("Add an expense")
+                        .setView(R.layout.add_dialog)
+                        .setNegativeButton("Cancel",null)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+
+                                EditText name = (EditText) ((Dialog)dialog).findViewById(R.id.dialogName);
+                                EditText amount = (EditText) ((Dialog)dialog).findViewById(R.id.dialogAmount);
+
+
+                                ExpenseName.add(name.getText().toString());
+                                Expense.add(Integer.parseInt(amount.getText().toString()));
+                                //Expense.add(Integer.valueOf(amount.toString()));
+
+                                adapter.notifyDataSetChanged();
+                            }
+                        }).show();
+
+            }
+        });
 
         return v;
     }
