ca91f243b291216ec4fd15bb1114dfdef9031f54
Deven <deven9852@gmail.com>
2017-06-07 14:36:32 +0530

    Initial commit

---
+++ app/src/main/java/com/devapp/hp/expensemanagerrevamped/MainActivity.java
@@ -0,0 +1,14 @@
+package com.devapp.hp.expensemanagerrevamped;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Initial commit
+    }
+}
